598bd952adeee0dec02cbc65986a5be4f59367bd
Yuki AOI <ja01youki@gmail.com>
2020-10-05 16:37:01 +0900

    initialize

---
+++ appWindowsLocker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices; // DLL Import
+
+namespace appWindowsLocker
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            NativeMethods.LockWorkStation();
+        }
+    }
+    static class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        public static extern void LockWorkStation();
+    }
+}
